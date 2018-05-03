a39319d1df3ae2bf1ba6f27c1a122c41627c7f0b
Andrea Mauri <andrea.mauri87@gmail.com>
2018-05-03 09:35:37 +0200

    added conf file example

---
+++ configuration/configuration_example.py
@@ -0,0 +1,13 @@
+CLIENT_ID = ""
+CLIENT_SECRET = ""  # Fill this in with your client secret
+REDIRECT_URI = ""
+
+ACCESS_TOKEN = ""
+
+REDDIT_USER = ""
+REDDIT_PSW = ""
+
+
+DB_NAME = ""
+DB_PORT = 27017
+DB_HOST = ""
